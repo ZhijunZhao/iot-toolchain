f5bea3ffa0a1e14ed2f726fbed17216e5c6db62c
yungez <yungez@microsoft.com>
2017-02-23 14:58:28 +0800

    support extension arguments promote

--- extension.js
+++ extension.js
@@ -3,6 +3,14 @@ var execSync = require('child_process').execSync;
 var fs = require('fs');
 var path = require('path');
 var os = require('os');
+var vscode = require('vscode');
+
+var pickOptions = {
+    matchOnDescription: true,
+    placeHolder: "type device type"
+};
+
+const supportedDevices = [ 'raspberrypi', 'edison' ];
 
 function localExecCmd(cmd, args, outputChannel, cb) {
     try {
@@ -42,8 +50,7 @@ function cloneDockerRepo(context) {
     if (fs.existsSync(repoPath)) {
         console.log('repo already exists');
         try {
-            var cdCmd = os.platform() === 'win32' ? 'cd /d ' : 'cd ';
-            var log = execSync(cdCmd + repoPath + ' && git pull');
+            var log = execSync('cd ' + repoPath + ' && git pull');
             console.log(log);
         } catch (e) {
             console.log(e);
@@ -64,63 +71,102 @@ function activate(context) {
     console.log('Congratulations, your extension "azure-iot-development" is now active!');
 
     var outputChannel = vscode.window.createOutputChannel("Azure IoT build");
+
+    var properties = {};
+
     let build = vscode.commands.registerCommand('extension.build', function () {
-        cloneDockerRepo(context);
+        // get input argument : device
+        vscode.window.showQuickPick(supportedDevices, pickOptions)
+        .then(devicetype => {
+            if (!devicetype) return;
+            
+            // call command
+            console.log('get devicetype from user input');
+            cloneDockerRepo(context);
 
-        var configPath = path.join(vscode.workspace.rootPath, '/config.json');
-        if (fs.existsSync(configPath)) {
-            delete require.cache[configPath];
-            var config = require(configPath);
+            var configPath = path.join(vscode.workspace.rootPath, '/config.json');
+            if (fs.existsSync(configPath)) {
+                delete require.cache[configPath];
+                var config = require(configPath);
 
-            var repoName = 'iotdev-docker';
-            var repoPath = context.extensionPath + '/' + repoName;
-            var mainPath = os.platform() === 'win32' ? repoPath + '/main.bat' : repoPath + '/main.sh';
-            var workingdir = config.workingdir ? config.workingdir : vscode.workspace.rootPath;
-            var builddirname = config.builddir ? config.builddir : 'build';
-            var buildpath = path.join(workingdir, builddirname);
-            if (!fs.existsSync(buildpath)) {
-                fs.mkdirSync(buildpath);
-            }
+                var repoName = 'iotdev-docker';
+                var repoPath = context.extensionPath + '/' + repoName;
+                var mainPath = os.platform() === 'win32' ? repoPath + '/main.bat' : repoPath + '/main.sh';
+                var workingdir = config.workingdir ? config.workingdir : vscode.workspace.rootPath;
+                var builddirname = config.builddir ? config.builddir : 'build';
+                var buildpath = path.join(workingdir, builddirname);
+                if (!fs.existsSync(buildpath)) {
+                    fs.mkdirSync(buildpath);
+                }
+                
+                console.log(devicetype + '--' + workingdir + '--' + builddirname);
+                if (os.platform() === 'win32') {
+                    localExecCmd(mainPath, ['build', '--device', devicetype, '--workingdir', workingdir, '--builddir', builddirname], outputChannel);
+                } else {
+                    localExecCmd('bash', [mainPath, 'build', '--device', devicetype, '--workingdir', workingdir, '--builddir', builddirname], outputChannel);
+                }
 
-            console.log(config.device + '--' + workingdir + '--' + builddirname);
-            if (os.platform() === 'win32') {
-                localExecCmd(mainPath, ['build', '--device', config.device, '--workingdir', workingdir, '--builddir', builddirname], outputChannel);
+                properties.device = devicetype;
             } else {
-                localExecCmd('bash', [mainPath, 'build', '--device', config.device, '--workingdir', workingdir, '--builddir', builddirname], outputChannel);
+                vscode.window.showErrorMessage('config.json does not exist.');
+                console.log('config file does not exist');
             }
-        } else {
-            vscode.window.showErrorMessage('config.json does not exist.');
-            console.log('config file does not exist');
-        }
+        });
     });
 
     let deploy = vscode.commands.registerCommand('extension.deploy', function () {
-        cloneDockerRepo(context);
+        var devicetype = '';
+        var ipaddress = '';
+        var username = '';
+        var password = '';
 
-        var configPath = path.join(vscode.workspace.rootPath, '/config.json');
-        if (fs.existsSync(configPath)) {
-            delete require.cache[configPath];
-            var config = require(configPath);
+        vscode.window.showQuickPick(supportedDevices, pickOptions)
+        .then(selected => {
+            if (!selected) return;
+            devicetype = selected;        
+            
+            return vscode.window.showInputBox( { prompt: "Please enter your device IP" } )
+            }).then(ip => {
+                ipaddress = ip;
+                return vscode.window.showInputBox( { prompt: "Please enter your device username", placeHolder: "root" } )
+            }).then(user => {
+                username = user
+                return vscode.window.showInputBox( { prompt: "Please enter your device password", placeHolder: "password", password: true } )
+            }).then(pw => {
+                password = pw;
+         
+                cloneDockerRepo(context);
 
-            var repoName = 'iotdev-docker';
-            var repoPath = context.extensionPath + '/' + repoName;
-            var mainPath = os.platform() === 'win32' ? repoPath + '/main.bat' : repoPath + '/main.sh';
-            var workingdir = config.workingdir ? config.workingdir : vscode.workspace.rootPath;
-            var srcpath = config.srcpath ? config.srcpath : vscode.workspace.rootPath;
-            var destdir = config.destdir ? config.destdir : '/home/' + config.username;
-            if (os.platform() === 'win32') {
-                localExecCmd(mainPath, ['deploy', '--device', config.device, '--workingdir', workingdir,
-                    '--deviceip', config.deviceip, '--username', config.username, '--password', config.password,
-                    '--srcpath', srcpath, '--destdir', destdir], outputChannel);
+                var configPath = path.join(vscode.workspace.rootPath, '/config.json');
+                if (fs.existsSync(configPath)) {
+                    delete require.cache[configPath];
+                    var config = require(configPath);
+
+                    var repoName = 'iotdev-docker';
+                    var repoPath = context.extensionPath + '/' + repoName;
+                    var mainPath = os.platform() === 'win32' ? repoPath + '/main.bat' : repoPath + '/main.sh';
+                    var workingdir = config.workingdir ? config.workingdir : vscode.workspace.rootPath;
+                    var srcpath = config.srcpath ? config.srcpath : vscode.workspace.rootPath;
+                    var destdir = config.destdir ? config.destdir : '/home/' + config.username;
+                    if (os.platform() === 'win32') {
+                    //localExecCmd(mainPath, ['deploy', '--device', config.device, '--workingdir', workingdir,
+                        //'--deviceip', config.deviceip, '--username', config.username, '--password', config.password,
+                        //'--srcpath', srcpath, '--destdir', destdir], outputChannel);
+                    localExecCmd(mainPath, ['deploy', '--device', devicetype, '--workingdir', workingdir,
+                        '--deviceip', ipaddress, '--username', username, '--password', password,
+                        '--srcpath', srcpath, '--destdir', destdir], outputChannel);
+                    } else {
+                        localExecCmd('bash', [mainPath, 'deploy', '--device', devicetype, '--workingdir', workingdir,
+                        '--deviceip', ipaddress, '--username', username, '--password', password,
+                        '--srcpath', srcpath, '--destdir', destdir], outputChannel);
+                }
+
+                properties.device = devicetype;
             } else {
-                localExecCmd('bash', [mainPath, 'deploy', '--device', config.device, '--workingdir', workingdir,
-                    '--deviceip', config.deviceip, '--username', config.username, '--password', config.password,
-                    '--srcpath', srcpath, '--destdir', destdir], outputChannel);
+                vscode.window.showErrorMessage('config.json does not exist.');
+                console.log('config file does not exist');
             }
-        } else {
-            vscode.window.showErrorMessage('config.json does not exist.');
-            console.log('config file does not exist');
-        }
+        })             
     });
 
     context.subscriptions.push(build);
